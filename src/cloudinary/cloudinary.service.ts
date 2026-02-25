0cefb7b4989318479b151c4eb0c7a6daf6eaaf85
Dubrelh <henrykemeni0@gmail.com>
2026-02-25 03:13:37 +0100

    cloudinary video config

--- src/cloudinary/cloudinary.service.ts
+++ src/cloudinary/cloudinary.service.ts
@@ -12,13 +12,16 @@ export class CloudinaryService {
     file: Express.Multer.File,
   ): Promise<UploadApiResponse | UploadApiErrorResponse> {
     return new Promise((resolve, reject) => {
-      const upload = cloudinary.uploader.upload_stream((error, result) => {
-        // eslint-disable-next-line @typescript-eslint/prefer-promise-reject-errors
-        if (error) return reject(error);
-        if (!result)
-          return reject(new Error('Cloudinary upload result is undefined'));
-        resolve(result);
-      });
+      const upload = cloudinary.uploader.upload_stream(
+        { resource_type: 'auto' },
+        (error, result) => {
+          // eslint-disable-next-line @typescript-eslint/prefer-promise-reject-errors
+          if (error) return reject(error);
+          if (!result)
+            return reject(new Error('Cloudinary upload result is undefined'));
+          resolve(result);
+        },
+      );
 
       // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
       toStream(file.buffer).pipe(upload);
