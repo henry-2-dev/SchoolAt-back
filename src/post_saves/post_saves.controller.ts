1abfb9ae65baf620380fa0ffc458fafde0d39b47
Dubrelh <henrykemeni0@gmail.com>
2026-03-26 18:56:04 +0100

    save ajust

--- src/post_saves/post_saves.controller.ts
+++ src/post_saves/post_saves.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { PostSavesService } from './post_saves.service';
 
 @Controller('post-saves')
